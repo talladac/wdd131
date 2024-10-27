82cbf386ed2d2af3520a667a3d854179b7b1275a
talladac <figuracionc@gmail.com>
2024-10-27 04:48:18 +0400

    final project

--- project/scripts/final-project.js
+++ project/scripts/final-project.js
@@ -14,3 +14,21 @@ document.addEventListener('DOMContentLoaded', function() {
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 document.getElementById("lastmodified").innerHTML = document.lastModified;
 })
+
+const placeContainers = document.querySelectorAll("place-container");
+const nxtBtn = document.querySelectorAll(".nxt-btn");
+const preBtn = document.querySelectorAll(".pre-btn");
+
+placeContainers.forEach((item, i) =>{
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', ()=>{
+        item.scrollLeft -= containerWidth;
+    })
+})
